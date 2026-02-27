d7c9606060b4ba7f716bda5b34cb225732f9d228
puru sharma <purusharma@purus-MacBook-Air.local>
2026-02-27 15:23:50 +0530

    fix: TurboReactPackage for New Architecture

--- android/src/main/java/com/reactnativebadgemodule/BadgePackage.kt
+++ android/src/main/java/com/reactnativebadgemodule/BadgePackage.kt
@@ -1,16 +1,28 @@
 package com.reactnativebadgemodule
 
-import com.facebook.react.ReactPackage
+import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.uimanager.ViewManager
 
-class BadgePackage : ReactPackage {
-  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-    return listOf(BadgeModule(reactContext))
-  }
+class BadgePackage : TurboReactPackage() {
 
-  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-    return emptyList()
-  }
+    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+        return if (name == BadgeModule.NAME) BadgeModule(reactContext) else null
+    }
+
+    override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
+        mapOf(
+            BadgeModule.NAME to ReactModuleInfo(
+                BadgeModule.NAME,
+                BadgeModule.NAME,
+                false,
+                false,
+                false,
+                true  // isTurboModule
+            )
+        )
+    }
 }
